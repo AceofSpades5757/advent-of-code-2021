898cec20318390d673aea1dbd28d13a2367567c3
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2021-12-03 00:49:22 -0600

    Finish setup

--- puzzle_1.py
+++ puzzle_1.py
@@ -1,8 +1,29 @@
-import urllib.request
+import configparser
+from pathlib import Path
+from typing import Iterator
 
 
-input_url: str = 'https://adventofcode.com/2021/day/1/input'
-request: urllib.request.Request = urllib.request.Request(input_url)
+CONFIG_FILE = Path(__file__).parent / 'config.ini'
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE)
 
-with urllib.request.urlopen(request) as response:
-    input_data: str = response.read().decode('utf-8')
+
+def get_input_data() -> Iterator[str]:
+
+    data_path: Path = Path(config.get('paths', 'data'))
+    data_file: Path = data_path / 'puzzle_1.txt'
+
+    if not data_path.exists():
+        data_path.mkdir()
+
+    with open(data_file, 'r') as fin:
+        for line in fin:
+            yield line.strip()
+
+
+if __name__ == '__main__':
+
+    input_lines: Iterator[str] = get_input_data()
+
+    for line in input_lines:
+        print(line)

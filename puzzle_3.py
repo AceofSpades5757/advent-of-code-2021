0455d2b7535084603902eb682ba4ab83b5ca7757
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2021-12-03 20:33:03 -0600

    Add puzzle 3

---
+++ puzzle_3.py
@@ -0,0 +1,43 @@
+""" Given list of commands, what is your final depth times your final
+horizontal position. """
+import configparser
+from pathlib import Path
+from typing import Final
+from typing import Iterator
+
+
+CONFIG_FILE: Final[Path] = Path(__file__).parent / 'config.ini'
+config: configparser.ConfigParser = configparser.ConfigParser()
+config.read(CONFIG_FILE)
+
+
+def get_input_data() -> Iterator[str]:
+
+    data_path: Path = Path(config.get('paths', 'data'))
+    data_file: Path = data_path / 'input_2.txt'
+
+    if not data_path.exists():
+        data_path.mkdir()
+
+    with open(data_file, 'r') as fin:
+        for line in fin:
+            yield line.strip()
+
+
+if __name__ == '__main__':
+
+    input_lines: Iterator[str] = get_input_data()
+
+    horizontal_position, depth = 0, 0
+    for command in input_lines:
+        match command.split():
+            case 'forward', value:
+                horizontal_position += int(value)
+            case 'up', value:
+                depth -= int(value)
+            case 'down', value:
+                depth += int(value)
+            case _:
+                raise ValueError(f'Unknown command: {command.split()}')
+
+    print(f'ANSER: {horizontal_position * depth}')
